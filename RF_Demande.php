02a17620f4bf872c57473c46cfc52e87bb88471b
motardx <55079871+motardx@users.noreply.github.com>
2019-12-02 22:13:59 +0100

    Add files via upload

--- RF_Demande.php
+++ RF_Demande.php
@@ -1,46 +1,44 @@
-<!DOCTYPE html>
 
-<html>
-    <head>
-        <title>REFERENT DEMANDES</title>
         <?php
         include_once 'head.php';
+        include_once 'bdd.php';
         ?>
-    </head>
     
     <body>
-        <h1>Demandes de Réservation</h1>
+        <br><br><h1 class="text-center" >Demandes de Réservation</h1><br><br><br>
         
-        <table>
+        <table class="table table-striped table-bordered table-hover">
+        <thead class="thead-dark">
             <tr>
-                <th>Nom Etudiant</th>
-                <th>Classe</th>
-                <th>Salle</th>
-                <th>Date</th>
-                <th>Heure</th>
-                <th>Commentaire</th>
+                <th>Nom Etudiant </th>
+                <th>Classe </th>
+                <th>Salle </th>
+                <th>Date </th>
+                <th>Heure </th>
+                <th>Commentaire </th> 
                 <th>Décision</th>
             </tr>
-            
-            <tr>
-                <td>BON Killian</td>
-                <td>SIO</td>
-                <td>Fu24</td>
-                <td>22/11/2019</td>
-                <td>13h-14h</td>
-                <td>Je souhaite reserve</td>
-                <td>
-                    <form action="/action_page.php">
-                        <input type="checkbox" name="valider" value="Valider"> Valider<br>
-                        <input type="checkbox" name="refuser" value="Refuser"> Refuser<br>
-                        <label for="message">Justification : </label><br>
-                        <textarea rows="5" cols="70" type="text" name="justification" id="justification" required ></textarea><br>
-                        <input type="submit" value="Confirmer">
-                    </form>
-                </td>
-            </tr>
-        </table>
+        </thead>
+     
         
+            <?php
+                $statement=$db -> prepare("SELECT * FROM BOOKING INNER JOIN ACCOUNT ON BOOKING.idBooking = ACCOUNT.idAccount"
+                . " INNER JOIN TIMESLOT ON BOOKING.idTimeSlot = TIMESLOT.idTimeSlot INNER JOIN CLASS ON ACCOUNT.idClass = CLASS.idClass INNER JOIN ROOM ON BOOKING.idBooking = ROOM.idRoom ");
+                $statement -> execute();
+                    while($row = $statement->fetch()) {
+                        echo "<tr><td>".$row['firstName']." ".$row['lastName']." "."</td>";
+                        echo "<td>".$row['shortName']." "."</td>";
+                        echo "<td>".$row['name']." "."</td>";
+                        echo "<td>".$row['date']." "."</td>";
+                        echo "<td>".$row['start']." ".$row['end']."</td>";
+                        echo "<td>".$row['commentaire']." "."</td>";
+                        echo "<td>".'<form action="/action_page.php">'.'<input type="checkbox" name="valider" value="Valider"> Valider'.'<br>'.
+                            '<input type="checkbox" name="refuser" value="Refuser"> Refuser'.'<br>'.'<label for="message">Justification : </label>'.'<br>'.
+                            '<textarea rows="2" cols="30" type="text" name="justification" id="justification" required >'.'</textarea><br>'.
+                            '<input type="submit" value="Confirmer">'.'</form></td></tr>';
+                    } 
+            ?>
+        </table>
     </body>
 </html> 
 
