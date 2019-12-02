0c5f907a06de9b06f9149571d799e92a787ab823
bonk79 <55079884+bonk79@users.noreply.github.com>
2019-12-02 20:04:35 +0100

    Remplissage du tableau avec la table BOOKING

--- RF_Demande.php
+++ RF_Demande.php
@@ -1,6 +1,6 @@
-<?php
+
+        <?php
         include_once 'head.php';
-        include_once 'bdd.php';
         ?>
     
     <body>
@@ -8,22 +8,32 @@
         
         <table>
             <tr>
-                <th>Nom Etudiant</th>
-                <th>Classe</th>
-                <th>Salle</th>
-                <th>Date</th>
-                <th>Heure</th>
-                <th>Commentaire</th>
+                <th>Nom Etudiant </th>
+                <th>Classe </th>
+                <th>Salle </th>
+                <th>Date </th>
+                <th>Heure </th>
+                <th>Commentaire </th> 
                 <th>Décision</th>
             </tr>
-            
+     
+                <?php
+    
+    $statement=$db -> prepare("SELECT * FROM BOOKING INNER JOIN ACCOUNT ON BOOKING.idBooking = ACCOUNT.idAccount"
+            . " INNER JOIN TIMESLOT ON BOOKING.idTimeSlot = TIMESLOT.idTimeSlot INNER JOIN CLASS ON ACCOUNT.idClass = CLASS.idClass INNER JOIN ROOM ON BOOKING.idBooking = ROOM.idRoom ");
+                        $statement -> execute();
+                        while($row = $statement->fetch()) {
+                            echo "<tr><td>".$row['firstName']." ".$row['lastName']." "."</td>";
+                             echo "<td>".$row['shortName']." "."</td>";
+                             echo "<td>".$row['name']." "."</td>";
+                             echo "<td>".$row['date']." "."</td>";
+                            echo "<td>".$row['start']." ".$row['end']."</td>";
+                           echo "<td>".$row['commentaire']." "."</td></tr>";
+                        } 
+    
+    
+    ?>
             <tr>
-                <td>BON Killian</td>
-                <td>SIO</td>
-                <td>Fu24</td>
-                <td>22/11/2019</td>
-                <td>13h-14h</td>
-                <td>Je souhaite reserve</td>
                 <td>
                     <form action="/action_page.php">
                         <input type="checkbox" name="valider" value="Valider"> Valider<br>
@@ -33,7 +43,7 @@
                         <input type="submit" value="Confirmer">
                     </form>
                 </td>
-            </tr>
+        </tr>
         </table>
         
     </body>
